27ac0ec364cbc8c5880614aaf589be438bf31503
Questioillustro <sxb7781@g.rit.edu>
2012-09-25 08:12:01 -0400

    Test

---
+++ AlphaTeam/src/HTMLConstruct_Iterator.java
@@ -0,0 +1,17 @@
+class HTMLConstruct_Iterator {
+	
+	private HTMLConstruct head, current;
+	
+	public HTMLConstruct_Iterator(HTMLConstruct h) {
+		head = h;
+		current = head;
+	}
+	
+	public String toString() {
+		String temp = "";
+		int tabCount = 0;
+			
+		
+		return temp;
+	}
+}
---
+++ AlphaTeam/src/HTMLHeadTag.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HTMLHeadTag implements HTMLConstruct {
+
+	private String tagName;
+	private String sTag;
+	private String eTag;
+	private String text;
+	private ArrayList<HTMLConstruct> childConstructs = new ArrayList<HTMLConstruct>();
+	
+	public HTMLHeadTag(){
+		tagName = "h1";
+		sTag = "<h1>";
+		eTag = "</h1>";
+		text = "";
+	}
+	
+	//Adds a child construct
+	public void add(HTMLConstruct construct){
+		childConstructs.add(construct);
+	}
+	
+	//Removes a child construct
+	public void remove(HTMLConstruct construct){
+		childConstructs.remove(construct);
+	}
+
+	//Sets the text in between the HTML tags
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	//Gets the text in between the HTML tags
+	public String getText() {
+		return text;
+	}
+
+	//Gets the tag name (identifier for the type of tag)
+	public String getTagName() {
+		return tagName;
+	}
+
+	//Gets the start tag
+	public String getSTag() {
+		return sTag;
+	}
+
+	//Gets the end tag
+	public String getETag() {
+		return eTag;
+	}
+
+	@Override
+	public ArrayList<HTMLConstruct> getChild() {
+		return childConstructs;
+	}
+
+}
