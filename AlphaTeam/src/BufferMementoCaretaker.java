b3e0cbf473527edc33fdcea5c9af36dd82b6886b
mhickson <mhickson@android-d77c368462ac690.wireless.rit.edu>
2012-10-20 21:04:42 -0400

    initial BufferMementoCaretaker without methods

---
+++ AlphaTeam/src/BufferMementoCaretaker.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+import java.util.ArrayList;
+/**
+ * @author Maddison Hickson
+ *
+ */
+public class BufferMementoCaretaker {
+	private ArrayList<BufferMemento> mementoStack;
+	
+	public BufferMementoCaretaker(){
+		mementoStack = new ArrayList<BufferMemento>();	
+	}
+	
+}
