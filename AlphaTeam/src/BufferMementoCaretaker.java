6d3f7986d4d64af133c7906a4e2b1874655c30da
mhickson <mhickson@android-d77c368462ac690.wireless.rit.edu>
2012-10-20 21:18:28 -0400

    adds methods with functionality and documentation

--- AlphaTeam/src/BufferMementoCaretaker.java
+++ AlphaTeam/src/BufferMementoCaretaker.java
@@ -13,4 +13,28 @@ public class BufferMementoCaretaker {
 		mementoStack = new ArrayList<BufferMemento>();	
 	}
 	
+	/**
+	 * adds a memento for the caretaker to hold. If more than two states are
+	 * already being stored then the oldest if removed, which is the first
+	 * in the ArrayList.
+	 * 
+	 * @param bufferState, the new memento
+	 */
+	public void addMemento(BufferMemento bufferState){
+		mementoStack.add(bufferState);
+		if(mementoStack.size() > 2){
+			mementoStack.remove(0);
+		}
+	}
+	
+	/**
+	 * Gives the last memento the caretaker was given. Also removes the last
+	 * memento the caretaker was given.
+	 * @return lastState, the last memento the caretaker was given
+	 */
+	public BufferMemento getLastState(){
+		BufferMemento lastState = mementoStack.get(mementoStack.size()-1);
+		mementoStack.remove(mementoStack.size()-1);
+		return lastState;
+	}
 }
