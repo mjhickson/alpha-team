546fcb067d26121239485ae3df9301bd11e67e48
Michael Yeaple <mdy7643@g.rit.edu>
2012-10-21 12:27:57 -0400

    HTML Composite re-worked to adhere to DRY principles.

--- AlphaTeam/src/HTMLConstruct.java
+++ AlphaTeam/src/HTMLConstruct.java
@@ -2,78 +2,116 @@ import java.util.ArrayList;
 
 /**
  * HTMLConstruct.java
- * 
- * An interface for the HTMLConstruct Objects. This is the Component part
- * of the Composite pattern.
- * The HTMLConstruct classes that implement this interface can all 
- * be either leaves or composites.
  */
 
 /**
- * An interface for the HTMLConstruct Objects. This is the Component part
+ * Defines an interface for the HTMLConstruct Objects. This is the Component part
  * of the Composite pattern.
- * The HTMLConstruct classes that implement this interface can all 
- * be either leaves or composites.
+ * Implements default behavior for all concrete constructs.
+ * All children of HTMLConstruct may either be leaves or composites
+ * in the Composite pattern.
  * 
  * @author Michael Yeaple
  */
 
-public interface HTMLConstruct {
+public class HTMLConstruct {
+	
+	protected String tagName;
+	protected String sTag;
+	protected String eTag;
+	protected String text;
+	protected ArrayList<HTMLConstruct> childConstructs = new ArrayList<HTMLConstruct>();
+	
+	public HTMLConstruct(){
+		// HTMLConstructs should not actually be used. This simply defines the default methods
+		// and variables to be inherited by all concrete constructs.
+	}
 	
 	/**
 	 * Adds a child HTMLConstruct.
 	 * @param HTMLConstruct construct
 	 */
-	public void add(HTMLConstruct construct);
+	public void add(HTMLConstruct construct){
+		childConstructs.add(construct);
+	}
 	
 	/**
 	 * Removes a childHTMLConstruct.
 	 * @param HTMLConstruct construct
 	 */
-	public void remove(HTMLConstruct construct);
-	
+	public void remove(HTMLConstruct construct){
+		childConstructs.remove(construct);
+	}
+
 	/**
 	 * Sets the text in between HTML Tags.
 	 * @param String text
 	 */
-	public void setText(String text);
+	public void setText(String text) {
+		this.text = text;
+	}
 	
 	/**
 	 * Returns the text in between the HTML Tag (ex: <b>This part of the tag.</b>).
 	 * @return String text
 	 */
-	public String getText();
-	
+	public String getText() {
+		return text;
+	}
+
 	/**
 	 * Returns the tag name (identifier for the HTML tag).
 	 * @return String tagName
 	 */
-	public String getTagName();
-	
+	public String getTagName() {
+		return tagName;
+	}
+
 	/**
 	 * Return the start tag of a construct (ex: <b>).
 	 * @return String sTag
 	 */
-	public String getSTag();
-	
+	public String getSTag() {
+		return sTag;
+	}
+
 	/**
 	 * Returns the end tag of a construct (ex: </b>).
 	 * @return String eTag
 	 */
-	public String getETag();
-	
+	public String getETag() {
+		return eTag;
+	}
+
 	/**
 	 * Returns an ArrayList of the child tags.
 	 * @return ArrayList<HTMLConstruct> childConstructs
 	 */
-	public ArrayList<HTMLConstruct> getChild();
+	public ArrayList<HTMLConstruct> getChild() {
+		return childConstructs;
+	}
 	
 	/**
 	 * Recursive function to get the HTML tag composite structure as a String.
 	 * @param html - the HTML as a string.
-	 * @param isIndenting - boolean for whether the structure should be indented or not.
-	 * @param iNum - int for the number of indents before the tag should be inserted (initial value should be 0).
 	 * @return String html
 	 */
-	public String getHTML(String html, boolean isIndenting, int iNum);
+	public String getHTML(String html){
+		html += sTag;
+		
+		if ( childConstructs.size() == 0){
+			html += text;
+		} else{
+			
+			for ( int i = 0; i < childConstructs.size(); i++){
+				html += childConstructs.get(i).getHTML(html);
+			}
+			
+		}
+		
+		html += eTag;
+		
+		return html;
+	}
+	
 }
