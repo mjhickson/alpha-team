b68c40ecfb6565f31f74b85eec7955895e7369bd
Michael Yeaple <mdy7643@g.rit.edu>
2012-10-21 22:43:51 -0400

    Added recursive indentation functionality to HTML constructs.

--- AlphaTeam/src/HTMLConstruct.java
+++ AlphaTeam/src/HTMLConstruct.java
@@ -16,10 +16,12 @@ import java.util.ArrayList;
 
 public class HTMLConstruct {
 	
-	protected String tagName;
-	protected String sTag;
-	protected String eTag;
-	protected String text;
+	protected String tagName; //Name of the tag (i.e. A, IMG, etc.)
+	protected String sTag; //Start tag (i.e. <h1>, <b>, etc.)
+	protected String eTag; //End tag (i.e. </h1>, </b>, etc.)
+	protected String text; //Text contained within the tag (Text the user writes within a tag).
+	protected int indentNum; //Number of times to indent before writing the tag.
+	//Children of tag, if applicable.
 	protected ArrayList<HTMLConstruct> childConstructs = new ArrayList<HTMLConstruct>();
 	
 	public HTMLConstruct(){
@@ -49,6 +51,14 @@ public class HTMLConstruct {
 	 */
 	public void setText(String text) {
 		this.text = text;
+	}
+	
+	/**
+	 * Sets the number of indentations before a tag is inserted.
+	 * @param num
+	 */
+	public void setIndentNum(int num){
+		indentNum = num;
 	}
 	
 	/**
@@ -82,6 +92,14 @@ public class HTMLConstruct {
 	public String getETag() {
 		return eTag;
 	}
+	
+	/**
+	 * Returns the number of indentations before a tag is inserted.
+	 * @return indentNum
+	 */
+	public int getIndentNum(){
+		return indentNum;
+	}
 
 	/**
 	 * Returns an ArrayList of the child tags.
@@ -92,26 +110,52 @@ public class HTMLConstruct {
 	}
 	
 	/**
+	 * Increases all indentation from this structure on by the passed in integer.
+	 */
+	public void increaseIndent(int num){
+		indentNum += num;
+		
+		//Call the function recursively for all children of the construct.
+		for ( int i = 0; i < childConstructs.size(); i++){
+			childConstructs.get(i).increaseIndent(num);
+		}
+	}
+	
+	/**
 	 * Recursive function to get the HTML tag composite structure as a String.
 	 * @param html - the HTML as a string.
 	 * @return String html
 	 */
-	public String getHTML(String html){
-		html += sTag;
+	public String getHTML(String html, boolean isIndenting){
 		
-		if ( childConstructs.size() == 0){
-			html += text;
-		} else{
-			
-			for ( int i = 0; i < childConstructs.size(); i++){
-				html += childConstructs.get(i).getHTML(html);
-			}
-			
+		if(html != ""){
+			//If this is not the first tag in the string, make a new line.
+			html += "\n";
 		}
 		
-		html += eTag;
+		if (isIndenting){
+			//Indent the number of times applicable for the tag, if applicable.
+			for ( int i = 0; i < indentNum; i++ ){
+				html += "\t";
+			}
+		}
+		
+		html += sTag + text; //Add the start tag and text to the string.
+		
+		//Call the function recursively for all children of the construct.
+		for ( int i = 0; i < childConstructs.size(); i++ ){
+			html += childConstructs.get(i).getHTML(html, isIndenting);
+		}
+		
+		if (isIndenting){
+			//Indent the number of times applicable before inserting the end tag.
+			for ( int i = 0; i < indentNum; i++ ){
+				html += "\t";
+			}
+		}
+		
+		html += eTag; //Add the end tag to the string.
 		
 		return html;
-	}
-	
+	}	
 }
